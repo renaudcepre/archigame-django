03f733cd362ec2af26423f9b57ba33c39f2d7688
rcepre <renaudcepre@gmail.com>
2024-02-21 22:14:21 +0100

    login page

--- users/models.py
+++ users/models.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 from games.models import Game, PlayerScore
 
 
 class User(AbstractUser):
-    def score(self, game_pk):
-        game = Game.objects.get(pk=game_pk)
-        total_score = PlayerScore.objects.filter(game=game, player=self).aggregate(total=Sum('score'))['total']
-        return total_score if total_score is not None else 0
+    coucou = models.IntegerField(default=0)
+
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
 
 @login_required
-def profile_view(request):
+def profile_view(request: WSGIRequest):
+    print("sdfhoiuh")
     return render(request, 'users/profile.html', {'user': request.user})
