d48de13be5b33db327be172c59c576e08fb48448
rcepre <renaudcepre@gmail.com>
2024-02-22 20:48:24 +0100

    ajout total score

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.db.models import Sum
+from .models import User, UserGameScore
+
+# Définition d'une nouvelle classe d'admin pour User
+class UserAdmin(BaseUserAdmin):
+    def total_score(self, obj):
+        return UserGameScore.objects.filter(user=obj).aggregate(total=Sum('total_score'))['total'] or 0
+
+    total_score.short_description = 'Total Score'  #
+
+    list_display = BaseUserAdmin.list_display + ('total_score',)
+
 
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Sum
 
-from games.models import Game, PlayerScore
+from games.models import Game, PlayerScore, UserGameScore
 
 
 class User(AbstractUser):
     coucou = models.IntegerField(default=0)
+
+    def total_score(self):
+        return UserGameScore.objects.filter(user=self).aggregate(total=Sum('total_score'))['total'] or 0
 
