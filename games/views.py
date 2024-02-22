cf8e70edb6755e4e80e85b946be87f4fd9cb6a04
rcepre <renaudcepre@gmail.com>
2024-02-22 22:24:04 +0100

    cache game list for leaderboard

--- games/views.py
+++ games/views.py
@@ -3,16 +3,24 @@ from django.shortcuts import render
 
 from .models import Game, UserGameScore
 
+from django.core.cache import cache
+
+
+def get_cached_game_list():
+    game_list = cache.get('game_list')
+    if game_list is None or len(game_list) == 0:
+        games_with_scores = Game.objects.annotate(num_scores=Count('usergamescore')).filter(num_scores__gt=0)
+        game_list = list(games_with_scores)
+        cache.set('game_list', game_list, timeout=None)  # Aucune expiration du cache
+    return game_list
+
 
 def leaderboards(request):
-    count = Game.objects.annotate(num_scores=Count('usergamescore')).filter(num_scores__gt=0).count()
-
-    if count > 0:
-        game = Game.objects.annotate(num_scores=Count('usergamescore')).filter(num_scores__gt=0).order_by('?')[
-               :1].get()
+    game_list = get_cached_game_list()
+    if game_list:
+        game = game_list.pop(0)
+        # Met à jour la liste des jeux en cache
+        cache.set('game_list', game_list, timeout=None)  # Aucune expiration du cache
         leaderboard = UserGameScore.objects.filter(game=game).order_by('-total_score')[:10]
-    else:
-        game = None
-        leaderboard = []
 
     return render(request, 'leaderboards.html', {'game': game, 'leaderboard': leaderboard})
