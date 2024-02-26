cfbaea46c2eb16c149d56a6a3cad8d3c54a4ec61
rcepre <renaudcepre@gmail.com>
2024-02-26 11:17:20 +0100

    HAHAHAHHA

--- games/forms.py
+++ games/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from django.forms import inlineformset_factory
 
 from .models import Game, Extension
 from .models import GameConfiguration
@@ -25,9 +24,6 @@ class ExtensionForm(forms.ModelForm):
         fields = ['name', 'bgg_number']
 
 
-ExtensionFormSet = inlineformset_factory(Game, Extension, form=ExtensionForm, extra=1, can_delete=True)
-
-
 class GameConfigurationForm(forms.ModelForm):
     class Meta:
         model = GameConfiguration
@@ -38,17 +34,14 @@ class GameConfigurationForm(forms.ModelForm):
         game = cleaned_data.get("game")
         extensions = cleaned_data.get("extensions", [])
 
-        # Vérifie si chaque extension est liée au jeu spécifié.
         for extension in extensions:
             if extension.game != game:
                 raise ValidationError(f"L'extension '{extension.name}' n'est pas liée au jeu '{game.name}'.")
 
-        # Vérifie l'unicité de la combinaison jeu/extensions.
         if game:
-            existing_configs = GameConfiguration.objects.filter(game=game)
-            for config in existing_configs:
-                # Convertit QuerySet en list pour comparaison, car 'extensions' est une liste à ce moment.
-                if set(config.extensions.all()) == set(extensions):
-                    raise ValidationError("Une configuration identique pour ce jeu existe déjà.")
-
+            if not self.instance.pk:
+                existing_configs = GameConfiguration.objects.filter(game=game)
+                for config in existing_configs:
+                    if set(config.extensions.all()) == set(extensions):
+                        raise ValidationError("Une configuration identique pour ce jeu existe déjà.")
         return cleaned_data
--- games/urls.py
+++ games/urls.py
@@ -1,17 +1,3 @@
-from django.urls import path
-
-from games.views import GameCreateView, GameDetailView, GameUpdateView, game_list, leaderboards, leaderboard, add_play
-
 urlpatterns = [
-    # Crud
-    path('games/add', GameCreateView.as_view(), name='add_game'),
-    path('games/', game_list, name='game_list'),
-    path('games/update_game/<int:pk>/', GameUpdateView.as_view(), name='update_game'),
-    path('games/detail/<int:game_id>/', GameDetailView.as_view(), name='game_detail'),
-
-    path('plays/add/', add_play, name='add_play'),
-    # Leaderboards
-    path('leaderboards/', leaderboards, name='leaderboards'),
-    path('leaderboards/<int:game_id>', leaderboard, name='leaderboards_detail'),
 
 ]
